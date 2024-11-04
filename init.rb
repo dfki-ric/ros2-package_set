d96d6cfd3c2e2b8673484856f1bb3f3284ed9b2b
Steffen Planthaber <steffen.planthaber@dfki.de>
2024-11-04 14:57:55 +0100

    auto-import rosdeps

--- init.rb
+++ init.rb
@@ -1,25 +1,46 @@
 
 require_relative "./lib/colcon_package.rb"
 require_relative "./lib/colcon_import_package.rb"
+require_relative "./lib/rosdep_import.rb"
 
-os_names, os_versions = Autoproj.workspace.operating_system
-if os_names.include?('ubuntu')
-    if os_versions.include?('focal')
-        Autoproj.message ("Load ros2 osdeps with the suffix 'focal'")
-        Autoproj.workspace.osdep_suffixes << 'focal'
-        Autoproj.message Autoproj.workspace.osdep_suffixes
-    elsif os_versions.include?('jammy')
-        Autoproj.message ("Load ros2 osdeps with the suffix 'jammy'")
-        Autoproj.workspace.osdep_suffixes << 'jammy'
-    else
-        Autoproj.error("Unsupported ubuntu version #{is_versions}")
-    end
-else
-    Autoproj.error("Unsupported os version: #{os_names}")
+
+Autoproj.config.declare "ROS_VERSION",
+"string",
+default: "humble",
+doc: ["Which ros version should be used?"],
+options: ["humble", "iron", "foxy", "rolling"]
+
+prefix = Autoproj.manifest.package_set("ros2").local_dir
+ubuntu_osdeps = prefix+"/ubuntu.osdeps"
+ros_osdeps = prefix+"/ros.osdeps"
+if !File.exist?(ubuntu_osdeps) || !File.exist?(ros_osdeps) then
+    Autoproj.message "Importing rosdeps to " + prefix
+    importer = Ros2::RosdepImporter.new(ubuntu_osdeps, ros_osdeps)
+    importer.import(Autoproj.config.get("ROS_VERSION"))
 end
 
-ros_setup_bash = File.join(Autoproj.root_dir, '../install/setup.bash')
-if File.file?(ros_setup_bash)
-    Autoproj.env_source_file ros_setup_bash
-end
+
+
+# os_names, os_versions = Autoproj.workspace.operating_system
+# if os_names.include?('ubuntu')
+#     if os_versions.include?('focal')
+#         Autoproj.message ("Load ros2 osdeps with the suffix 'focal'")
+#         Autoproj.workspace.osdep_suffixes << 'focal'
+#         Autoproj.message Autoproj.workspace.osdep_suffixes
+        
+        
+#     elsif os_versions.include?('jammy')
+#         Autoproj.message ("Load ros2 osdeps with the suffix 'jammy'")
+#         Autoproj.workspace.osdep_suffixes << 'jammy'
+#     else
+#         Autoproj.error("Unsupported ubuntu version #{is_versions}")
+#     end
+# else
+#     Autoproj.error("Unsupported os version: #{os_names}")
+# end
+
+# ros_setup_bash = File.join(Autoproj.root_dir, '../install/setup.bash')
+# if File.file?(ros_setup_bash)
+#     Autoproj.env_source_file ros_setup_bash
+# end
 
