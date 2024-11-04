99774038bff079dead934b59f8ff5f10587cfea1
Steffen Planthaber <steffen.planthaber@dfki.de>
2024-11-04 16:57:03 +0100

    remove old handling form init.rb

--- init.rb
+++ init.rb
@@ -34,25 +34,6 @@ if ros_version != imported_ros_osdeps then
 end
 
 
-
-# os_names, os_versions = Autoproj.workspace.operating_system
-# if os_names.include?('ubuntu')
-#     if os_versions.include?('focal')
-#         Autoproj.message ("Load ros2 osdeps with the suffix 'focal'")
-#         Autoproj.workspace.osdep_suffixes << 'focal'
-#         Autoproj.message Autoproj.workspace.osdep_suffixes
-        
-        
-#     elsif os_versions.include?('jammy')
-#         Autoproj.message ("Load ros2 osdeps with the suffix 'jammy'")
-#         Autoproj.workspace.osdep_suffixes << 'jammy'
-#     else
-#         Autoproj.error("Unsupported ubuntu version #{is_versions}")
-#     end
-# else
-#     Autoproj.error("Unsupported os version: #{os_names}")
-# end
-
 # ros_setup_bash = File.join(Autoproj.root_dir, '../install/setup.bash')
 # if File.file?(ros_setup_bash)
 #     Autoproj.env_source_file ros_setup_bash
