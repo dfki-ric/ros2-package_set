ffc73009a9f08555c60bc885a0fdb8784ca9a572
Christoph Hertzberg <christoph.hertzberg@dfki.de>
2026-09-14 18:30:43 +0200

    Factor out common code from colcon[_import]_package activate `add_cmake_arg` functionality to colcon_package as well

--- lib/colcon_package.rb
+++ lib/colcon_package.rb
@@ -1,3 +1,5 @@
+require_relative "colcon_common.rb"
+
 def colcon_package(name, type = :import_package, importdir: name, move_to: name, workspace: Autoproj.workspace)
     send(type, name) do |pkg|
         pkg.depends_on 'python3-colcon-common-extensions'
@@ -14,20 +16,15 @@ def colcon_package(name, type = :import_package, importdir: name, move_to: name,
             pkg.importdir = importdir
         end
 
-        pkg.post_install do |pkg|
-            colcon_dir =  File.expand_path(workspace.root_dir + "/../")
-            if (Autoproj.config.get("COLCON_USE_SYMLINK_INSTALL") == true) then
-                pkg.progress_start "building %s using colcon --symlink-install in #{colcon_dir}" do
-                    Autobuild::Subprocess.run(pkg, 'build', 'colcon', 'build', '--symlink-install','--event-handlers', 'console_direct+', '--packages-select', pkg.name, :working_directory => colcon_dir)
-                end
-            else
-                pkg.progress_start "building %s using colcon in #{colcon_dir}" do
-                    Autobuild::Subprocess.run(pkg, 'build', 'colcon', 'build','--event-handlers', 'console_direct+', '--packages-select', pkg.name, :working_directory => colcon_dir)
-                end
-            end
-        end
+        colcon_build_hook(pkg, workspace)
 
         yield(pkg) if block_given?
+
+        # write the colcon.pkg file after the block, so cmake args added by a
+        # pkg.add_cmake_arg() call in the block are picked up
+        pkg.post_import do
+            pkg.write_colcon_pkg_file()
+        end
     end
 
     if name != move_to then
